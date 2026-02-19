5e737f7d58630b8668f562b6f3e47808d753aca5
Hampiz <hampus.bengtsson@elev.ga.ntig.se>
2026-02-19 13:57:04 +0100

    Lektion 3

--- app.rb
+++ app.rb
@@ -5,38 +5,42 @@ require 'sinatra/reloader'
 require 'bcrypt'
 
 get('/') do
+    slim(:index)
+end
+
+get('/grocerylist') do
     query = params['q']
-    db = SQLite3::Database.new('grocerylist.db')
-    edit_id = params['edit_id']
+    db = SQLite3::Database.new('db/grocerylist.db')
+    edit_id = params[:edit_id]
     db.results_as_hash = true
-    @grocerylist = db.execute('SELECT * FROM grocerylist')
     if query && !query.empty?
         @grocerylist = db.execute("SELECT * FROM grocerylist WHERE name LIKE ?", "%#{query}%")
     else
         @grocerylist = db.execute('SELECT * FROM grocerylist')
     end
-    slim(:index)
+    slim(:grocerylist)
 end
 
 post('/grocerylist') do
-    new_crocery = params['new_grocery']
+    new_grocery = params['new_grocery']
     description = params[:description]
     store = params[:store]
-    db = SQLite3::Database.new('grocerylist.db')
-    db.execute('INSERT INTO grocerylist (name, description, store) VALUES (?, ?, ?)', [new_crocery, description, store])
+    db = SQLite3::Database.new('db/grocerylist.db')
+    db.execute('INSERT INTO grocerylist (name, description, store) VALUES (?, ?, ?)', [new_grocery, description, store])
     redirect('/grocerylist')
 end
 
 post('/grocerylist/:id/update') do
-        #plocka upp id, name och description
-        id = params[:id].to_i
-        name = params[:name]
-        store = params[:store]
-        #koppla till databasen
-        db = SQLite3::Database.new("db/grocerylist.db")
-        db.execute("UPDATE grocerylist SET name = ?, store = ? WHERE id = ?",[name,store,id])
-        #slutligen, redirect till grocerylist sidan
-        redirect('/grocerylist')
+    # plocka upp id, name, description och store
+    id = params[:id].to_i
+    name = params[:name]
+    description = params[:description]
+    store = params[:store]
+    # koppla till databasen
+    db = SQLite3::Database.new('db/grocerylist.db')
+    db.execute('UPDATE grocerylist SET name = ?, description = ?, store = ? WHERE id = ?', [name, description, store, id])
+    # slutligen, redirect till grocerylist sidan
+    redirect('/grocerylist')
 end
 
 post('/grocerylist/:id/delete') do
@@ -55,9 +59,10 @@ post('/grocerylist/:id/done') do
     redirect('/grocerylist')
     end
 
+
 post('/grocerylist/:id/undone') do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/grocerylist.db")
     db.execute("UPDATE grocerylist SET state = 0 WHERE id = ?", id)
     redirect('/grocerylist')
-    end
+end
